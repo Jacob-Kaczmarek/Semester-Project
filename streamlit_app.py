03489359dd509ce658c75a40253aaab14f4d0fc4
Jacob-Kaczmarek <jkaczmarek@unomaha.edu>
2024-12-09 12:59:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-# Install necessary libraries
-!pip install pandas requests streamlit
+
 
 # Import libraries
 import pandas as pd
